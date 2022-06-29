8c0e5838347bb26d04bd036fa8e954dd8f283d20
Tell396 <vova21473@gmail.com>
2022-06-30 00:22:04 +1000

    Story #13. Todo list in README file added.

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -128,7 +128,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "870cb1996084e4f8"; }
+/******/ 		__webpack_require__.h = function() { return "10acb9355db283e2"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
